5189b6e219907fbf736642f1084102c16b6be170
YuraGavriley <yura.gavriley@gmail.com>
2024-11-08 18:26:47 +0200

    Create register, login endpoints

--- main.py
+++ main.py
@@ -1,11 +1,39 @@
-from fastapi import FastAPI
-from .database import create_db_and_tables
-from contextlib import asynccontextmanager
+from fastapi import FastAPI, Depends, HTTPException
+from . import schemas, crud, auth, utils
+from sqlmodel import Session
+from .database import create_db_and_tables, engine
+# from contextlib import asynccontextmanager
 
 
-@asynccontextmanager
-async def lifespan(app: FastAPI):
+# @asynccontextmanager
+# async def lifespan(app: FastAPI):
+#     create_db_and_tables()
+#     yield
+
+# app = FastAPI(lifespan=lifespan)
+
+app = FastAPI()
+
+@app.on_event("startup")
+def on_startup():
     create_db_and_tables()
-    yield
 
-app = FastAPI(lifespan=lifespan)
+def get_db():
+    db = Session(engine)
+    try:
+        yield db
+    finally:
+        db.close()
+
+@app.post("/register", response_model=schemas.UserResponse)
+def register(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    db_user = crud.create_user(db, user)
+    return db_user
+
+@app.post("/login")
+def login(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    db_user = crud.get_user_by_email(db, user.email)
+    if not db_user or not utils.verify_password(user.password, db_user.hashed_password):
+        raise HTTPException(status_code=400, detail="Invalid credentials")
+    token = auth.create_access_token({"sub": user.email})
+    return {"access_token": token, "token_type": "bearer"}
