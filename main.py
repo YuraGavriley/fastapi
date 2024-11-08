77e02afb86d978645fc3242a3a1de6f17c86bc2e
YuraGavriley <yura.gavriley@gmail.com>
2024-11-08 13:07:28 +0200

    Add database creation

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
 from fastapi import FastAPI
+from .database import create_db_and_tables
+from contextlib import asynccontextmanager
 
-app = FastAPI()
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    create_db_and_tables()
+    yield
+
+app = FastAPI(lifespan=lifespan)
