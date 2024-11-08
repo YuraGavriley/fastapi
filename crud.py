e4b53eb49a3023c61d08feb28f943e3776559846
YuraGavriley <yura.gavriley@gmail.com>
2024-11-08 17:35:43 +0200

    Add create user crud operation

---
+++ crud.py
@@ -0,0 +1,11 @@
+from sqlmodel import Session
+from .schemas import UserCreate
+from .models import User
+from .utils import hash_password
+
+def create_user(db: Session, user: UserCreate):
+    db_user = User(email=user.email, hashed_password=hash_password(user.password))
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
