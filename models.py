01c218798d2be035a1c26be26474938e8af1524a
YuraGavriley <yura.gavriley@gmail.com>
2024-11-08 12:45:18 +0200

    Replaced SQLAlchemy with SQLModel

--- models.py
+++ models.py
@@ -1,11 +1,7 @@
-from sqlalchemy import Column, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
+from sqlmodel import SQLModel, Field
 
-Base = declarative_base()
-
-class User(Base):
-
+class User(SQLModel, table=True):
     __tablename__ = "users"
-    id = Column(Integer, primary_key=True, index=True)
-    email = Column(String, unique=True, index=True)
-    hashed_password = Column(String)
+    id: int = Field(default=None, primary_key=True, index=True)
+    email: str = Field(unique=True, index=True)
+    hashed_password: str
